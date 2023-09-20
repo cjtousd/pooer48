711b0b2c147787c8b96affd0f32b0c9e890bbc1b
cjtousd <jimenatousdiaz@gmail.com>
2023-09-19 21:39:01 -0500

    23 CAP4

---
+++ poopro23/src/poopro23/Poopro23.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poopro23;
+
+
+public class Poopro23 {
+
+
+    public static void main(String[] args) {
+        
+        Formulario formulario = new Formulario();
+        formulario.setVisible(true);
+    }
+    
+}
